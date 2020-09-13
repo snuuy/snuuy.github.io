0a5b86f4f58afbb5a1910fff57d4ca55d98e6aca
sunnybh <sunnynbhatt@gmail.com>
2020-09-13 18:17:30 -0400

    Add anchor to open first project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(() => {
-    if (window.location.hash == "#portfolio") {
+    if (window.location.hash == "#portfolio" || window.location.hash == "#cpcheats") {
         $("#portfolio").slideToggle()
         $("#toggleIcon").toggleClass("fa-chevron-up")
+    }
+
+    if (window.location.hash == "#cpcheats") {
+        $('#proj1').modal('toggle')
     }
 
     $("#toggleBtn").click(() => {
